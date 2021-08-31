3da9edda7978db0fff49508065771755bc020bc4
Muhammed Ahad <maaahad@Muhammeds-MacBook-Pro.local>
2021-08-31 23:27:19 +0200

    navigation styles started with Sass

--- components/nav.js
+++ components/nav.js
@@ -1,5 +1,5 @@
 // react
-
+import React, { useState, useReducer } from "react";
 // next
 
 // react-icons
@@ -8,20 +8,61 @@ import { BiMoon, BiSun, BiCloud, BiData, BiLogInCircle } from "react-icons/bi";
 
 // components
 
+// sass stylesheets
+import Styles from "../styles/nav.module.sass";
+
 export default function Nav() {
+  const [lighttheme, toggleTheme] = useReducer(
+    (lighttheme) => !lighttheme,
+    true
+  );
+  const [localStorage, toggleStorage] = useReducer(
+    (localStorage) => !localStorage,
+    true
+  );
   return (
-    <nav>
-      <div>
+    <nav className={Styles.container}>
+      <div className={Styles.logo}>
         <h4>MA-Library</h4>
       </div>
-      <div>
-        <div>
-          <BiCloud />
-          <BiData />
+      <div className={Styles.right_nav}>
+        <div className={Styles.storage}>
+          <input
+            type="checkbox"
+            checked={localStorage}
+            onChange={() => toggleStorage()}
+            id="storage"
+          />
+          <label for="storage">
+            {localStorage ? (
+              <div>
+                <BiCloud />
+              </div>
+            ) : (
+              <div>
+                <BiData />
+              </div>
+            )}
+          </label>
         </div>
-        <div>
-          <BiSun />
-          <BiMoon />
+        <div className={Styles.theme}>
+          <input
+            type="checkbox"
+            checked={lighttheme}
+            onChange={() => toggleTheme()}
+            id="theme"
+          />
+          <label for="theme">
+            {lighttheme ? (
+              <div>
+                <BiSun />
+              </div>
+            ) : (
+              <div>
+                <BiMoon />
+              </div>
+            )}
+          </label>
         </div>
         <div>
           <span>Login</span>
