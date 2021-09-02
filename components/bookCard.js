fee6bcfc2668d3e39a01dbc600e41f26590e1616
Muhammed Ahad <maaahad@Muhammeds-MacBook-Pro.local>
2021-09-02 17:11:42 +0200

    BookCard added ... TODO: Work with the data now

--- components/bookCard.js
+++ components/bookCard.js
@@ -1,6 +1,15 @@
 // react
 
 // next
+import Image from "next/image";
+// react icons
+import {
+  BiEdit,
+  BiBookReader,
+  BiLike,
+  BiMessageRounded,
+  BiTrash,
+} from "react-icons/bi";
 
 // sass styles
 import Styles from "../styles/bookCard.module.sass";
@@ -9,13 +18,39 @@ function CardHeader() {
   return (
     <div className={Styles.cardHeader}>
       <div className={Styles.cardMeta}>
-        <h4>Title</h4>
+        <h4>JavaScript The Definitive Guide </h4>
         <h6>Author name</h6>
         <h6>page, year</h6>
       </div>
-      <div>
-        <div>Read</div>
-        <div>Edit</div>
+      <div className={Styles.cardEdit}>
+        {/* the following mught be handled with checkbox,  */}
+        {/* So that we can target checked state to style in CSS */}
+        <div className={Styles.status}>
+          {/* read book : green color, unread-book: red color */}
+          <BiBookReader />
+        </div>
+        <div className={Styles.edit}>
+          <BiEdit />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function CardActions() {
+  return (
+    <div className={Styles.cardActions}>
+      <div className={Styles.likeComment}>
+        <div>
+          <BiLike />
+        </div>
+        <div>
+          <BiMessageRounded />
+        </div>
+      </div>
+      <div className={Styles.removeMe}>
+        <BiTrash />
+        <span>Remove Me</span>
       </div>
     </div>
   );
@@ -25,11 +60,10 @@ export default function BookCard() {
   return (
     <div className={Styles.bookContainer}>
       <CardHeader />
-      <div className={Styles.cardMedia}>Book's Image</div>
-      <div className={Styles.cardActions}>
-        <div>Like and comments</div>
-        <div>Delete</div>
+      <div className={Styles.cardMedia}>
+        <Image src="/nocoverb.png" layout="fill" objectFit="fill" />
       </div>
+      <CardActions />
     </div>
   );
 }
