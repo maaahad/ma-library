946d92b10a3bff7db9a5f3fcecc985a590bfbafd
Muhammed Ahad <maaahad@Muhammeds-MacBook-Pro.local>
2021-09-11 23:47:16 +0200

    Summary added with useLibrarySummary hook. ISSUE: book status toggle in AddBook Form in both desktop version and edit option in each book

--- components/summary.js
+++ components/summary.js
@@ -1,23 +1,76 @@
 // react
-import React, { useRef } from "react";
+import React, { useRef, useEffect, useLayoutEffect } from "react";
 // nextjs
+
+// in-house hooks
+import { useLibrary } from "./libraryProvider";
 
 // sass styles
 import Styles from "../styles/summary.module.sass";
 
+// a custom hook for library summary
+const useLibrarySummary = () => {
+  const { library } = useLibrary();
+  const totalBooks = library.length;
+  let booksRead = 0;
+  library.forEach((book) => {
+    book.readStatus && booksRead++;
+  });
+  const booksNotRead = totalBooks - booksRead;
+
+  return [totalBooks, booksRead, booksNotRead];
+};
+
 export default function Summary() {
-  // let, total: 200, read: 25, not read 175
-  const statusBarRef = useRef();
+  const [totalBooks, booksRead, booksNotRead] = useLibrarySummary();
+  const statusBarContainerRef = useRef();
+
+  useEffect(() => {
+    const statusBarContainer = statusBarContainerRef.current;
+    if (booksRead) {
+      statusBarContainer.children[0].style.display = "flex";
+      statusBarContainer.children[0].style.height = "100%";
+      statusBarContainer.children[0].style.width = `${
+        (booksRead * 100) / totalBooks
+      }%`;
+      statusBarContainer.children[0].style.backgroundColor = "#48e98a";
+    } else {
+      statusBarContainer.children[0].style.display = "none";
+    }
+
+    if (booksNotRead) {
+      statusBarContainer.children[1].style.display = "flex";
+      statusBarContainer.children[1].style.height = "100%";
+      statusBarContainer.children[1].style.width = `${
+        (booksNotRead * 100) / totalBooks
+      }%`;
+      statusBarContainer.children[1].style.backgroundColor = "#fe4551";
+    } else {
+      statusBarContainer.children[1].style.display = "none";
+    }
+  });
 
   return (
     <div className={Styles.summaryContainer}>
-      <div ref={statusBarRef} className={Styles.statusBar}>
-        This is status bar
+      <div className={Styles.barExplanation}>
+        <div className={Styles.readBlock}>
+          <p>Read</p>
+          <div>{/* <span>{booksRead}</span> */}</div>
+        </div>
+        <div className={Styles.totalBlock}>
+          <p>Total Books</p>
+          <div>
+            <span>{totalBooks}</span>
+          </div>
+        </div>
+        <div className={Styles.notReadBlock}>
+          <p>Not Read</p>
+          <div>{/* <span>{booksNotRead}</span> */}</div>
+        </div>
       </div>
-      <div>
-        <div>read color</div>
-        <div>total Books</div>
-        <div>Not read color</div>
+      <div ref={statusBarContainerRef} className={Styles.statusBarContainer}>
+        <div title={booksRead}>{booksRead}</div>
+        <div title={booksNotRead}>{booksNotRead}</div>
       </div>
     </div>
   );
